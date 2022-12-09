8089c21be1e3fc2d73d860f3c676ec2067209a88
Diogo Pimenta <106586929+Di0g0Pimenta@users.noreply.github.com>
2022-12-09 19:35:55 +0000

    web
    redirecionar pagina

--- Scripts/api-riot-games.js
+++ Scripts/api-riot-games.js
@@ -1,17 +1,36 @@
-let key = "?api_key=RGAPI-f4c4201d-5ac0-44ca-8375-da4a3b292b90";
+let key = "?api_key=RGAPI-101b7e98-cfe7-4b47-bccf-2f9034a58090";
 let urlAPI = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
 var form = document.getElementById('form-sumuner');
 var sumenerName = document.getElementById('text');
 
-form.addEventListener('submit', function(e) {
+function submition(e) {
+    e.preventDefault();
+
     let urlFinish = urlAPI + sumenerName.value.toLowerCase() + key;
     fetch(urlFinish)
         .then(response => response.json())
         .then(response => {
         console.log(response)
-        alert(`O seu nickname é ${response.name}` + ` e o seu nivel é ${response.summonerLevel}!`)
+        //alert(`O seu nickname é ${response.name}` + ` e o seu nivel é ${response.summonerLevel}!`)
+        location.href = "http://127.0.0.1:5500/Stats%20Page/index-stats.html" + "?name=" + sumenerName.value.toLowerCase() ;
         });
-    
-    // impede o envio do form
+};
+
+
+
+
+
+/*form.addEventListener('submit', function(e) {
+    let urlFinish = urlAPI + sumenerName.value.toLowerCase() + key;
     e.preventDefault();
+    fetch(urlFinish)
+        .then(response => response.json())
+        .then(response => {
+        //console.log(response)
+        //alert(`O seu nickname é ${response.name}` + ` e o seu nivel é ${response.summonerLevel}!`)
+        document.getElementById("userName").innerHTML = response.name;
+        });
+        
+    
 });
+*/
