f655382a46cb6bc040b522d240892dcde91f9d80
Diogo Pimenta <106586929+Di0g0Pimenta@users.noreply.github.com>
2022-12-12 21:57:19 +0000

    Web
    Testes key

--- Scripts/api-riot-games.js
+++ Scripts/api-riot-games.js
@@ -1,4 +1,4 @@
-let key = "?api_key=RGAPI-101b7e98-cfe7-4b47-bccf-2f9034a58090";
+let key = "?api_key=RGAPI-fe1e9ec8-2293-4087-a511-b07cec521135";
 let urlAPI = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
 var form = document.getElementById('form-sumuner');
 var sumenerName = document.getElementById('text');
@@ -11,11 +11,13 @@ function submition(e) {
         .then(response => response.json())
         .then(response => {
         console.log(response)
-        //alert(`O seu nickname é ${response.name}` + ` e o seu nivel é ${response.summonerLevel}!`)
         location.href = "http://127.0.0.1:5500/Stats%20Page/index-stats.html" + "?name=" + sumenerName.value.toLowerCase() ;
         });
 };
 
+function back() {
+    location.href = "http://127.0.0.1:5500/Main%20Page/Index-main.html"
+}
 
 
 
